5b1ee2e15de37bb659b26dc572687f4af4c922cc
Donghyun Lee <ldhhello@naver.com>
2025-08-13 17:34:38 +0900

    401 Unauthorized

--- src/middlewares/jwtVerifier.ts
+++ src/middlewares/jwtVerifier.ts
@@ -1,18 +1,19 @@
 import { Request, Response, NextFunction } from 'express';
 import { verifyJwtToken } from '../utils/jwt';
+import HTTPError from '../utils/HTTPError';
 
 export default function jwtVerifier(req: Request, res: Response, next: NextFunction) {
     const authorization = req.headers.authorization;
 
     if (authorization === undefined) {
-        return next(new Error("Unauthorized"));
+        return next(new HTTPError(401, "Unauthorized"));
     }
     
     let token = authorization.split(' ')[1];
 
     let verifyResult = verifyJwtToken(token);
     if (!verifyResult) {
-        return next(new Error("Unauthorized"));
+        return next(new HTTPError(401, "Unauthorized"));
     }
 
     const uid = verifyResult;
