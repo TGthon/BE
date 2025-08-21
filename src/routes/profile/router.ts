ed4bb35d1443cf2f3c63b87086debbaffd68e5a9
Donghyun Lee <ldhhello@naver.com>
2025-08-22 01:37:08 +0900

    ✨ 프로필사진 url 던져주는 기능 추가

--- src/routes/profile/router.ts
+++ src/routes/profile/router.ts
@@ -41,6 +41,7 @@ router.put('/me/picture',
 
             let processed = false;
             let promises: Promise<any>[] = []; // 어차피 하나밖에 없긴 하지만 없을때 처리가 이게 더 편해서 일케 함!
+            const fileID = v4();
 
             busboy.on('file', (fieldname, file, fileinfo) => {
                 if (fieldname != 'picture' || fileinfo.mimeType != 'image/jpeg') {
@@ -52,22 +53,19 @@ router.put('/me/picture',
                     return;
                 }
 
-                processed = true;
+                promises.push(pipeline(
+                    file,
+                    sharp().resize(500, 500)
+                    .toFormat('jpeg', { quality: 70 }),
+                    fs.createWriteStream(`${process.env.IMAGE_FOLDER_PATH}/${fileID}.jpg`)
+                ));
 
-                // todo
-                promises.push((async () => {
-                    await pipeline(
-                        file,
-                        sharp().resize(500, 500)
-                        .toFormat('jpeg', { quality: 70 }),
-                        fs.createWriteStream(`${process.env.IMAGE_FOLDER_PATH}/test.jpg`)
-                    );
-                })());
+                processed = true;
             });
             busboy.on('close', async () => {
                 await Promise.all(promises);
                 if (processed) {
-                    res.status(200).json({ ok: true });
+                    res.status(200).json({ picture: `https://api.ldh.monster/images/${fileID}.jpg` });
                 }
                 else {
                     next(new HTTPError(400, "Bad request"));
