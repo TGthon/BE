a4b5cdc53fd725d8d4e1af9463e76e2da2f651e9
Donghyun Lee <ldhhello@naver.com>
2025-08-22 16:19:55 +0900

    ✨ calendar에 예시 색깔 추가

--- src/routes/calendar/service.ts
+++ src/routes/calendar/service.ts
@@ -5,6 +5,7 @@ type Calendar = {
     name: string,
     groupName?: string,
     users: string[],
+    color: string,
 }
 
 export const getCalendar = async (uid: number, year: string | undefined, month: string | undefined): Promise<Calendar[]> => {
@@ -13,14 +14,16 @@ export const getCalendar = async (uid: number, year: string | undefined, month: 
             start: new Date(2025, 8-1, 13, 12, 0, 0).getTime() / 1000,
             end: new Date(2025, 8-1, 13, 14, 0).getTime() / 1000,
             name: "테스트 1",
-            users: ["테스트", "테스트"]
+            users: ["테스트", "테스트"],
+            color: "#F59CA9"
         },
         {
             start: new Date(2025, 8-1, 14, 17, 0, 0).getTime() / 1000,
             end: new Date(2025, 8-1, 14, 18, 30).getTime() / 1000,
             name: "테스트 2",
             groupName: "테스트 그룹",
-            users: []
+            users: [],
+            color: "#8B5CF6"
         },
     ]
 }
