516316d06339bddf9219518042a9263207cca0d0
Donghyun Lee <ldhhello@naver.com>
2025-08-23 23:33:56 +0900

    ✨ 일정 추가

--- src/routes/calendar/service.ts
+++ src/routes/calendar/service.ts
@@ -1,5 +1,5 @@
 import { db } from '../../database';
-import { calendar, schedules } from '../../drizzle/schema';
+import { calendar, schedules, usersSchedules } from '../../drizzle/schema';
 import mysql from 'mysql2/promise';
 
 type Calendar = {
@@ -31,6 +31,8 @@ export const getCalendar = async (uid: number, year: string | undefined, month: 
     ]
 }
 
+// 여기에 그룹추가 하는 거는
+// event에 있는 거 그대로 가져와야겠다
 export async function createCalendar({
     uid,
     name,
@@ -46,7 +48,7 @@ export async function createCalendar({
     end: number;
     color?: string;
     note?: string;
-    users?: string[];
+    users: number[];
 }) {
     // DB에는 #떼고 저장
     if (color && color[0] == '#')
@@ -64,12 +66,23 @@ export async function createCalendar({
 
     // const insertId = result[0].id;
     // return { id: insertId, name, start, end, color, note, users };
-    await db.insert(schedules).values({
+    let result = await db.insert(schedules).values({
         // gid,
         name,
         start: new Date(start * 1000),
         end: new Date(end * 1000),
         color: color ?? '3B82F6',
         note: note ?? ''
-    });
+    }).$returningId();
+    const scheduleid = result[0].scheduleid;
+    users.push(uid);
+    
+    await db.insert(usersSchedules).values(
+        users.map(u => ({
+            uid: u,
+            scheduleid
+        }))
+    )
+
+    return { scheduleid };
 }
